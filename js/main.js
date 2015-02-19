ebfe0ed1336b87824ebaf8246602d71ad4431f30
jgagarin <jon.gagarin@gmail.com>
2015-02-19 13:08:32 -0800

    js files

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    
+    //add items with click
+    $('#submit-text').on('click', function(){
+        var itemVal = $('#textbox').val();
+        var itemText = '<li class="items active"><span>'+itemVal+'</span><div class="remove-item"><span>✗</span></div>'+'<div class="check"><span >✔</span></div></li>';
+        $('.items-list').append(itemText);
+    });
+    
+    //remove items with click
+    $(document).on('click','.remove-item', function(){
+        $(this).closest('li.items').fadeOut(400);
+    });
+    
+    //mark items as done with click
+    $(document).on('click','.check',function(){
+        $(this).closest('li.items').removeClass('active');
+        $(this).closest('li.items').addClass('done');
+        
+    });
+                   
+});
