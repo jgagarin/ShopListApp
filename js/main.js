d7d16bf194a51040e9f147e80ea672eb89ea13a4
jgagarin <jon.gagarin@gmail.com>
2015-02-19 13:36:05 -0800

    enter interactivity

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-    
+    //add items with enter
+    $('#textbox').keyup(function(event){
+        if(event.keyCode == 13){
+            event.preventDefault();
+            var itemVal = $('#textbox').val();
+        var itemText = '<li class="items active"><span>'+itemVal+'</span><div class="remove-item"><span>✗</span></div>'+'<div class="check"><span >✔</span></div></li>';
+        $('.items-list').append(itemText);
+        }
+    });
     //add items with click
     $('#submit-text').on('click', function(){
         var itemVal = $('#textbox').val();
